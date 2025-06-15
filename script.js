a02b2a226d322d4bf856637ac4c60e1b1ee947c1
Paullo2107 <80651575+Paullo2107@users.noreply.github.com>
2025-06-15 16:14:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const noticias = [
   {
     titulo: "Brasil avança em tecnologia sustentável",
     imagem: "https://github.com/Paullo2107/teste/tree/main/imagens/noticia1",
-    link: "https://g1.globo.com/brasil/noticia1",
+    link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjsr7qykfSNAxVLr5UCHSdTGL8QFnoECD0QAQ&url=https%3A%2F%2Fagenciagov.ebc.com.br%2Fnoticias%2F202505%2Fbrics-aprova-declaracao-e-avanca-em-agenda-industrial-sustentavel-com-foco-em-tecnologia-e-pequenas-empresas&usg=AOvVaw0AKOpVsNu5mIpleCEfFCO3&opi=89978449",
   },
   {
     titulo: "Nova descoberta na medicina impressiona cientistas",
     imagem: "https://github.com/Paullo2107/teste/tree/main/imagens/noticia2",
-    link: "https://g1.globo.com/saude/noticia2",
+    link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj138mekfSNAxUaR7gEHfLCBJwQFnoECB4QAQ&url=https%3A%2F%2Foglobo.globo.com%2Fsaude%2Fnoticia%2F2024%2F08%2F15%2Fcientistas-descobrem-que-humanos-envelhecem-drasticamente-em-dois-periodos-aos-44-anos-e-depois-dos-60-entenda.ghtml&usg=AOvVaw2Hvw1rv5ZIzx5pnk6CVx2R&opi=89978449",
   }
 ];
 
